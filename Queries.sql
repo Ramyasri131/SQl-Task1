c2156cef8479cb7cc88dbf9daa1fd638e3fbf7be
Ramyasri Sanaboina <ramyasri.s@Technovert.com>
2024-05-08 16:01:29 +0530

    Updating sql task

--- Queries.sql
+++ Queries.sql
@@ -8,7 +8,7 @@ select FirstName,Age,Salary from Employee;
 select FirstName as [Name] from Employee;
 
 --4. Select firstname and lastname as 'Name' for everyone. Use " " (space) to separate firstname and last
-select FirstName + ' ' + LastName as Name from Employee;
+select concat(FirstName , ' ' , LastName) as [Name] from Employee;
 
 --5. Select all columns for everyone with a salary over 38000. 
 select * from Employee where Salary>38000;
@@ -23,7 +23,7 @@ select FirstName,LastName,Salary from Employee where Title='Programmer';
 select * from Employee where LastName like '%o%';
 
 --9. Select the lastname for everyone whose first name equals "Kelly". 
-select LastName from Employee where FirstName like 'Kelly';
+select LastName from Employee where FirstName='Kelly';
 
 --10. Select all columns for everyone whose last name ends in "Moore".
 select * from Employee where LastName like '%Moore';
@@ -65,84 +65,85 @@ Select * from Employee where age<=34 order by Salary DESC;
 select * from Employee order by len(FirstName);
 
 --23. Select the number of employees whose age is above 45
-Select count(*) as NumberOfEmployees from Employee where age>45;
+Select count(*) as [NumberOfEmployees] from Employee where age>45;
 
 --24. Show the results by adding 5 to ages and removing 250 from salaries of all employees
-Select FirstName,LastName,Title,(Age+5) as Age , (Salary-250) as Salary from Employee;
+Select FirstName,LastName,Title,(Age+5) as [Age] , (Salary-250) as [Salary] from Employee;
 
 --25. Select the number of employees whose lastname ends with "re" or "ri" or "ks"
-Select Count(*) as count from Employee where LastName like '%re' or LastName like '%ri' or LastName like '%ks';
+Select Count(*) as [count] from Employee where LastName like '%re' or LastName like '%ri' or LastName like '%ks';
 
 --26. Select the average salary of all your employees
-Select avg(Salary) as AverageSalary from Employee;
+Select avg(Salary) as [AverageSalary] from Employee;
 
 --27. Select the average salary of Freshers
-Select avg(Salary) as AverageSalary from Employee where Title='Fresher';
+Select avg(Salary) as [AverageSalary] from Employee where Title='Fresher';
 
 --28. Select the average age of Programmers
-Select avg(Age) as AverageAge from Employee where Title='programmer';
+Select avg(Age) as [AverageAge] from Employee where Title='programmer';
 
 --29. Select the average salary of employees whose age is not less than 35 and not more than 50
-select avg(salary) as AverageSalary from Employee where Age between 35 and 50;
+select avg(salary) as [AverageSalary] from Employee where Age between 35 and 50;
 
 --30. Select the number of Freshers
-Select Count(*) as NumberOfFreshers from Employee where Title='Fresher';
+Select Count(*) as [NumberOfFreshers] from Employee where Title='Fresher';
 
 --31. What percentage of programmers constitute your employees
 select Count(*)*100/(select count(*) from Employee) as [percentage] from Employee where Title like 'programmer';
 
 --32. What is the combined salary that you need to pay to the employees whose age is not less than 40
-Select sum(Salary) as Total from Employee where Age>=40;
+Select sum(Salary) as [TotalSalary] from Employee where Age>=40;
 
 --33. What is the combined salary that you need to pay to all the Freshers and Programmers for 1 month
-Select Sum(Salary) as TotalSalary from Employee where Title like 'Fresher' or Title='programmer';
+Select Sum(Salary) as [TotalSalary] from Employee where Title='Fresher' or Title='programmer';
 
 --34. What is the combined salary that you need to pay to all the Freshers whose age is greater than 27 for 3years
-Select Sum(Salary)*3*12 as Salary from Employee where Title like 'Fresher' and Age>27;
+Select Sum(Salary)*3*12 as [TotalSalary] from Employee where Title='Fresher' and Age>27;
 
 --35. Select the eldest employee's firstname, lastname and age whose salary is less than 35000
 select FirstName,LastName,Age from Employee where Age in (select Max(Age) from Employee where Salary<35000);
+select Top 1 FirstName,LastName,Age from Employee where Salary<35000 order by Age Desc;
 
 --36. Who is the youngest General Manager
-Select * from Employee where Age in (select min(Age) from Employee where Title like 'Manager');
+Select * from Employee where Age in (select min(Age) from Employee where Title='Manager');
 
 --37. Select the eldest fresher whose salary is less than 35000
 select * from Employee where Age in (select Max(Age) from Employee where Title='Fresher' and Salary<35000);
 
 --38. Select firstname and age of everyone whose firstname starts with "John" or "Michael" and salary in the range 17000 and 26000
-select FirstName,Age from Employee where (FirstName like 'John' or FirstName like 'Micheal') and (Salary between 17000 and 26000);
-select FirstName,Age from Employee where (Salary between 17000 and 26000) and FirstName in (select FirstName from Employee where (FirstName like 'John' or FirstName like 'Micheal'));
+select FirstName,Age from Employee where (FirstName='John' or FirstName='Micheal') and (Salary between 17000 and 26000);
+select FirstName,Age from Employee where (Salary between 17000 and 26000) and FirstName in (select FirstName from Employee where (FirstName='John' or FirstName='Micheal'));
 
 --39. How many employees are having each unique title. Select the title and display the number of employees present in ascending order
-select Title,Count(Title) as NoOFEmployees from Employee group by Title;
+select Title,Count(Title) as [NoOFEmployees] from Employee group by Title order by Title;
 
 
 --40. What is the average salary of each unique title of the employees. Select the title and display the average salary of employees in each
-select Title,Avg(Salary) as AverageSalary from Employee group by Title;
+select Title,Avg(Salary) as [AverageSalary] from Employee group by Title;
 
 --41. What is the average salary of employees excluding Freshers
-select Title, Avg(Salary) as averageSalary from Employee where Title not like 'Fresher' group by Title;
+select Title, Avg(Salary) as [averageSalary] from Employee where Title!='Fresher' group by Title;
 
 --42. What is the average age of employees of each unique title.
-Select Title,Avg(Age) as AverageAge from Employee group by Title;
+Select Title,Avg(Age) as [AverageAge] from Employee group by Title;
 
 --43. In the age range of 25 to 40 get the number of employees under each unique title.
-select Title,Count(Title) as NoOfEmployees from Employee where Age between 25 and 40 group by Title;
+select Title,Count(Title) as [NoOfEmployees] from Employee where Age between 25 and 40 group by Title;
 
 --44. Show the average salary of each unique title of employees only if the average salary is not less than 25000
-Select Title,Avg(Salary) as AverageSalary from Employee Group by Title having Avg(Salary)>=25000;
+Select Title,Avg(Salary) as [AverageSalary] from Employee Group by Title having Avg(Salary)>=25000;
 
 --45. Show the sum of ages of each unique title of employee only if the sum of age is greater than 30
-Select Title,Sum(Age) as Age from Employee group by Title having sum(Age)>30;
+Select Title,Sum(Age) as [Age] from Employee group by Title having sum(Age)>30;
 
 --46.Lisa Ray just got married to Michael Moore. She has requested that her last name be updated to Moore.
-update Employee set LastName='Moore' where FirstName like 'Lisa' and LastName like 'Ray';
+update Employee set LastName='Moore' where FirstName='Lisa' and LastName='Ray';
 
 --47.Ginger Finger's birthday is today, add 1 to his age and a bonus of 5000
-update Employee set Age=Age+1 ,Salary+=5000 where FirstName like 'Ginger' and LastName like 'Finger';
+update Employee set Age=Age+1 ,Salary+=5000 where FirstName='Ginger' and LastName='Finger';
 
 --48.All 'Programmer's are now called "Engineer"s. Update all titles accordingly.
-update Employee set Title='Engineer' where Title like 'programmer';
+update Employee set Title='Engineer' where Title='programmer';
 
 --49.Everyone whose making under 30000 are to receive a 3500 bonus. 
 update Employee set Salary+=3500 where Salary<30000;
